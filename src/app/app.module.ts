e6bafd0e469e982507b471529e1958fd570bf0b6
omkar <omkardhapte99@gmail.com>
2023-05-31 21:37:09 +0530

    the basic fuction of Gradient tab
    only the ui pending

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { GradientComponent } from './gradient/gradient.component';
 import { HomeComponent } from './home/home.component';
 import { MulitpleColorGraditentComponent } from './mulitple-color-graditent/mulitple-color-graditent.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { GradientComponent } from './gradient/gradient.component';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -27,9 +29,11 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    ColorPickerModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/gradient/gradient.component.ts
+++ src/app/gradient/gradient.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./gradient.component.css']
 })
 export class GradientComponent {
+  selectedOption: string = '';
+  color1: string = '#ffffff';
+  color2: string = '#ffffff';
+  selectedColor: string = '';
+  degree: string = '';
+
+  options = [
+    { id: 1, name: 'Linear Gradient' },
+    { id: 2, name: 'Conic Gradient' },
+    { id: 3, name: 'Radial Gradient' },
+    { id: 4, name: 'Repeating Linear Gradient' }
+  ];
+
+  shapes = [
+    { id: 1, name: 'circle' },
+    { id: 2, name: 'ellipse' },
+    { id: 3, name: 'closest-side' },
+    { id: 4, name: 'closest-corner' },
+    { id: 4, name: 'farthest-side' },
+    { id: 4, name: 'farthest-corner' }
+  ];
+  onSelect(option: any) {
+    this.selectedOption = option.name;
+  }
+  getLinearGradient() {
+    this.selectedColor = `linear-gradient(${this.degree}deg, ${this.color1}, ${this.color2})`;
+  }
+
 
 }
